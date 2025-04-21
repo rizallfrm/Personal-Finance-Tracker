3ad4032592d2e126ac380e59fafcdb5c27e908b6
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-21 23:12:46 +0700

    feat: add index.js

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+// src/store/index.js
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import accountsReducer from './slices/accountsSlice';
+import transactionsReducer from './slices/transactionsSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    accounts: accountsReducer,
+    transactions: transactionsReducer
+  }
+});
+
+export default store;
