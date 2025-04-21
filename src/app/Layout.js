d406d0aaa4e7878b30c03282284a67ba03651879
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-21 23:12:07 +0700

    feat: add Layout redux

---
+++ src/app/Layout.js
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ReduxProvider } from '@/store/provider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
