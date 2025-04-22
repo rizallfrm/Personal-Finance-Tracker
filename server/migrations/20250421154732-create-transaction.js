ce19497454106a95293c08d1cc9a304d33753edb
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-22 18:51:49 +0700

    update: update migration models database and packages

--- server/migrations/20250421154732-create-transaction.js
+++ server/migrations/20250421154732-create-transaction.js
@@ -5,9 +5,9 @@ module.exports = {
     await queryInterface.createTable('Transactions', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
       },
       amount: {
         type: Sequelize.DECIMAL
